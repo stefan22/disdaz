8030a371321adfade442c95fd1245639d0dba792
Stefan Lissia <stefan.emailacct@gmail.com>
2015-07-16 00:02:52 +0100

    added 3 more tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,7 +9,25 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
 
-  it 'returns "fizzbuzz" when a multiple of 3 or 5' do
+  it 'returns "fizzbuzz" when passed 30' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
+
+  it 'returns 2 when passed 2' do
+    expect(fizzbuzz(2)).to eq 2
+  end
+
+  it 'returns "Please enter a number" when passed "Mike"' do
+    expect(fizzbuzz('Mike')).to eq 'Please enter a number'
+  end
+
+  it 'returns "Please enter a positive number" when passed -5' do
+    expect(fizzbuzz(-5)).to eq 'Please enter a positive number'
+  end
+
+  it 'returns "Please enter a number other than zero" when passed 0' do
+    expect(fizzbuzz(0)).to eq 'Please enter a number other than zero'
+  end
+
+
 end
